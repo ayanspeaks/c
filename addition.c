84e545c600fefc2f9e58f7559aa8b3f9249f30d8
ayanspeaks <97190014+ayanspeaks@users.noreply.github.com>
2022-01-13 23:55:22 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,sum;
+	printf("Enter two numbers for addition: ");
+	scanf("%f %f",&a, &b);
+	sum=a+b;
+	printf("Result of the addition is: %f",sum);
+	return 0;
+}
---
+++ subtraction.c
@@ -0,0 +1,10 @@
+
+int main()
+{
+	float a,b,sum;
+	printf("Enter two numbers for subtraction: ");
+	scanf("%f %f",&a, &b);
+	sum=a-b;
+	printf("Result of the subtraction is: %f",sum);
+	return 0;
+}
