7c649a29bc9c074a0434cb1dc8b09306986a5bd0
ayanspeaks <97190014+ayanspeaks@users.noreply.github.com>
2022-01-18 23:54:54 +0530

    Add files via upload

--- special_series.c
+++ special_series.c
@@ -2,10 +2,18 @@
 int main()
 {
 	int s=0,n,a;
+	float m;
 	printf("Enter a number: ");
-	scanf("%d",&n);
+	scanf("%f",&m);
+	if(m<0||floor(m)!=ceil(m))
+	{
+		printf("Wrong input.");
+		return 0;
+	}
+	n=m;
 	for(a=1; a <= n; a++) {
 		s += a * (a+1);
+		printf("Term %d of the series is: %d*%d\n",a,a,a+1);
 	}
 	printf("Result of the series is: %d",s);
 	return 0;
